e87e60062a8493b92f8acb01205dc6072bc43c76
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-14 00:06:20 -0800

    add case study component

--- frontend/app/_components/projects/CaseStudy.tsx
+++ frontend/app/_components/projects/CaseStudy.tsx
@@ -1,10 +1,35 @@
 import { CaseStudyType } from "@/app/_types";
 
 export function CaseStudy({ caseStudy }: { caseStudy: CaseStudyType }) {
-    const data = caseStudy
+
     return (
-        <div className="flex-shrink-0">
+        <section className="flex-shrink-0 mt-6">
+            <article>
+			    <h2>Problem</h2>
+                {caseStudy.problem}
+            </article>
             
-        </div>
+            <article>
+			    <h2>Cause & Impact</h2>
+                {caseStudy.cause}
+                {caseStudy.impact}
+            </article>
+
+            <article>
+                <h2>Solutions</h2>
+                {caseStudy.solution}
+                {caseStudy.alt_solution}
+            </article>
+
+            <article>
+                <h2>Challenges</h2>
+                {caseStudy.challenges}
+            </article>
+
+            <article>
+                <h2>Results</h2>
+                {caseStudy.results}
+            </article>
+        </section>
     )
 }
